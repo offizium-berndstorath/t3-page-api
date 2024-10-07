52d18cc8783a882f1c45ea36ea14f64b78531635
Bernd Storath <bernd.storath@offizium.de>
2024-10-07 09:49:36 +0200

    switch to nnrestapi

---
+++ Classes/Domain/Repository/EntryRepository.php
@@ -0,0 +1,4 @@
+<?php
+namespace Offizium\T3_page_api\Domain\Repository;
+
+class EntryRepository extends \TYPO3\CMS\Extbase\Persistence\Repository {}
---
+++ ext_localconf.php
@@ -0,0 +1,8 @@
+<?php
+defined('TYPO3') or die();
+
+call_user_func(
+	function( $extKey )
+	{
+	},
+'T3_page_api');
