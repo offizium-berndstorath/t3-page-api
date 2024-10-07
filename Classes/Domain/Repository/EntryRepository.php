cd4e1b3dcb9018afb53d38cb61ebf4167f7998f8
Bernd Storath <bernd.storath@offizium.de>
2024-10-07 10:23:29 +0200

    delete demo

--- Classes/Domain/Repository/EntryRepository.php
+++
@@ -1,4 +0,0 @@
-<?php
-namespace Offizium\T3pageapi\Domain\Repository;
-
-class EntryRepository extends \TYPO3\CMS\Extbase\Persistence\Repository {}
--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -1,4 +1,4 @@
 <?php
 defined('TYPO3') || die();
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('t3pageapi', 'Configuration/TypoScript', 'RestApi Example');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('t3pageapi', 'Configuration/TypoScript', 't3 page api');
